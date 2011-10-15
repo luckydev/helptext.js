67a97bf863d7fee196f0893ba267d84498bc532f
Anand <anand@tenmiles.com>
2011-10-15 22:51:54 +0530

    changed to optimized js

--- scripts/helptext.js
+++ scripts/helptext.js
@@ -4,33 +4,39 @@
     var $ele = this; 
 
     return this.each(function(){
-      $(this).data('helptext.help', $(this).attr('data-helptext'));
-      $(this).data('helptext.color', $(this).css('color'));
+        
+      var $t = $(this);
+      var $d = $t.attr('data-helptext');
+      var c  = $t.css('color');
+        
+      $t.data('helptext.help', $d);
+      $t.data('helptext.color', c);
 
       //if empty display the help text in gray
-      if($(this).val() == ''){
-        $(this).val($(this).data('helptext.help')).css('color', 'gray');
+      if($t.val() == ''){
+        $t.val($t.data('helptext.help')).css('color', 'gray');
       }
 
-      $(this).bind('focusin.helptext', function(){
+      $t.bind('focusin.helptext', function(){
         //here empty means val equals help text 
         //if empty clear out the help text and show cursor
-        if($(this).val() == $(this).data('helptext.help')){
-          $(this).val("").css('color', $(this).data('helptext.color'));
+        if($t.val() == $t.data('helptext.help')){
+          $t.val("").css('color', $(this).data('helptext.color'));
         }
       })
       .bind('focusout.helptext', function(){
         //if empty show help help text
-        if($(this).val() == ''){
-          $(this).val($(this).data('helptext.help')).css('color', 'gray');
+        if($t.val() == ''){
+          $t.val($t.data('helptext.help')).css('color', 'gray');
         }
       });
 
-      $(this).parents('form').bind('submit.helptext', function(){
+      $t.parents('form').bind('submit.helptext', function(){
         //before submit, if they are truly empty, make them empty
         $ele.each(function(){
-          if($(this).val() == $(this).data('helptext.help')){
-            $(this).val("").css('color', $(this).data('helptext.color'));
+          var $t = $(this);
+          if($t.val() == $t.data('helptext.help')){
+            $t.val("").css('color', $t.data('helptext.color'));
           }
         }); 
       });
